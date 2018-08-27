f57b36634e00459dc64733369b04651ac20d67a5
gaowh <gwh_2014@163.com>
2018-08-27 14:57:39 +0800

    Create index.js

---
+++ config/index.js
@@ -0,0 +1,51 @@
+
+
+// see http://vuejs-templates.github.io/webpack for documentation.
+var path = require('path')
+var baseUrl = 'http://127.0.0.1:9999';
+module.exports = {
+  build: {
+    env: require('./prod.env'),
+    index: path.resolve(__dirname, '../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    productionSourceMap: false,
+    // Gzip off by default as many popular static hosts such as
+    // Surge or Netlify already gzip all static assets for you.
+    // Before setting to `true`, make sure to:
+    // npm install --save-dev compression-webpack-plugin
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+    // Run the build command with an extra argument to
+    // View the bundle analyzer report after build finishes:
+    // `npm run build --report`
+    // Set to `true` or `false` to always turn it on or off
+    bundleAnalyzerReport: process.env.npm_config_report
+  },
+  dev: {
+    env: require('./dev.env'),
+    port: 8000,
+    autoOpenBrowser: true,
+    assetsSubDirectory: 'static',
+    host: 'localhost',
+    assetsPublicPath: '/',
+    proxyTable: {
+      '/auth': {
+        target: baseUrl,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/auth': '/auth'
+        }
+      },
+      '/admin': {
+        target: baseUrl,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/admin': '/admin'
+        }
+      }
+    },
+    cssSourceMap: false
+  }
+}
