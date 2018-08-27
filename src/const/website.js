05848038173482a63b3d87e822ffad7d0e803585
gaowenhui <gwh_2014@163.com>
2018-08-27 14:37:36 +0800

    gaowenhui20180827commit

--- src/const/website.js
+++ src/const/website.js
@@ -1,19 +1,4 @@
-/*
- *    Copyright (c) 2018-2025, lengleng All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- * Neither the name of the pig4cloud.com developer nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- * Author: lengleng (wangiegie@gmail.com)
- */
+
 
 export default {
   title: "Pig",
@@ -27,7 +12,7 @@ export default {
       ' 是一个基于vue+vuex+vue-router快速后台管理系统，采用token交互验证方式。',
       '最大程度上帮助企业节省时间成本和费用开支。 ',
       'QQ群：23754102',
-      '当前版本：v1.1.5'
+      '当前版本：v1.1.6'
     ]
   },
   wel: {
@@ -37,7 +22,7 @@ export default {
       ' 是一个基于vue+vuex+vue-router快速后台管理系统，采用token交互验证方式。',
       '最大程度上帮助企业节省时间成本和费用开支。 ',
       'QQ群：23754102',
-      '当前版本：v1.1.5'
+      '当前版本：v1.1.6'
     ]
   }
 }
